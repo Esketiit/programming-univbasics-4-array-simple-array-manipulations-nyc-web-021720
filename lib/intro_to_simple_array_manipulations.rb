89e0debd668d10ffb9dafb246e5b8d6f6547e410
Brian Desplantes <heftygac@gmail.com>
2020-02-07 16:21:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,8 +39,8 @@ def using_flatten(array)
   array.flatten
 end
 
-def using_delete(array,string)
-  array.delete()
+def using_delete(array, string)
+  array.delete(string)
 end
 
 def using_delete_at
